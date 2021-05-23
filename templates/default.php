f56c5a245f2dba22fb04dfa1c96cf55c72a25d4f
fwmakc <fwmakc@mail.ru>
2021-05-24 00:44:02 +0300

    1.0

---
+++ templates/default.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace is\Masters\Modules\Isengine\Currency;
+
+use is\Helpers\System;
+use is\Helpers\Objects;
+use is\Helpers\Strings;
+
+$instance = $object -> get('instance');
+$sets = &$object -> settings;
+$data = $object -> getData();
+
+if (!empty($data)) :
+	foreach ($data as $key => $item) :
+?>
+<div class="main-side__block-item main-side__block-item--currency">
+	<i class="fa fa-<?= $key; ?>" aria-hidden="true"></i>
+	<?= $item; ?>
+</div>
+<?php
+	endforeach;
+	unset($item, $key);
+endif;
+?>
+
+<?php if (!empty($sets['copyright'])) : ?>
+<small>
+	По данным сайта <a href="http://cbr.ru/" title="Центральный Банк России" target="_blank">Центробанка РФ</a>
+</small>
+<?php endif; ?>
