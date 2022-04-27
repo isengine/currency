b2b68bf9c9dc3099ab72529d99473ec7b1465d0d
fwmakc <fwmakc@mail.ru>
2022-04-27 11:29:20 +0300

    1.0

--- class/Currency.php
+++ class/Currency.php
@@ -10,50 +10,46 @@ use is\Helpers\Local;
 use is\Masters\Modules\Master;
 use is\Masters\View;
 
-class Currency extends Master {
-	
-	public function launch() {
-		
-		$sets = &$this -> settings;
-		
-		$url = 'http://www.cbr.ru/scripts/XML_daily.asp';
-		$data = [];
-		
-		$folder = $this -> cache;
-		$file = $folder . date('Ymd') . '.ini';
-		
-		Local::createFolder($folder);
-		
-		if (file_exists($file)) {
-			$xmlstr = Local::readFile($file);
-		} else {
-			$xmlstr = Local::requestUrl($url, null, 'post');
-			//$xmlstr = local::openUrl($url);
-			if (!empty($xmlstr)) {
-				Local::eraseFolder($folder);
-				Local::writeFile($file, $xmlstr);
-			}
-		}
-		unset($file, $url);
-		
-		if (!empty($xmlstr)) {
-			$valcurs = new \SimpleXMLElement($xmlstr);
-			foreach ($valcurs as $item) {
-				$item = (array) $item;
-				$val = strtolower($item['CharCode']);
-				if (in_array($val, $sets['currencies'])) {
-					$data[$val] = ceil(str_replace(',', '.', $item['Value']) * 100) / 100;
-				}
-				unset($val);
-			}
-			unset($valcurs, $item, $val);
-			$this -> setData($data);
-		}
-		
-		unset($xmlstr);
-		
-	}
-	
-}
+class Currency extends Master
+{
+    public function launch()
+    {
+        $sets = &$this->settings;
 
-?>
+        $url = 'http://www.cbr.ru/scripts/XML_daily.asp';
+        $data = [];
+
+        $folder = $this->cache;
+        $file = $folder . date('Ymd') . '.ini';
+
+        Local::createFolder($folder);
+
+        if (file_exists($file)) {
+            $xmlstr = Local::readFile($file);
+        } else {
+            $xmlstr = Local::requestUrl($url, null, 'post');
+            //$xmlstr = local::openUrl($url);
+            if (!empty($xmlstr)) {
+                Local::eraseFolder($folder);
+                Local::writeFile($file, $xmlstr);
+            }
+        }
+        unset($file, $url);
+
+        if (!empty($xmlstr)) {
+            $valcurs = new \SimpleXMLElement($xmlstr);
+            foreach ($valcurs as $item) {
+                $item = (array) $item;
+                $val = strtolower($item['CharCode']);
+                if (in_array($val, $sets['currencies'])) {
+                    $data[$val] = ceil(str_replace(',', '.', $item['Value']) * 100) / 100;
+                }
+                unset($val);
+            }
+            unset($valcurs, $item, $val);
+            $this->setData($data);
+        }
+
+        unset($xmlstr);
+    }
+}
--- templates/default.php
+++ templates/default.php
@@ -6,25 +6,25 @@ use is\Helpers\System;
 use is\Helpers\Objects;
 use is\Helpers\Strings;
 
-$instance = $this -> get('instance');
-$sets = &$this -> settings;
-$data = $this -> getData();
+$instance = $this->get('instance');
+$sets = &$this->settings;
+$data = $this->getData();
 
 if (!empty($data)) :
-	foreach ($data as $key => $item) :
+    foreach ($data as $key => $item) :
 ?>
 <div class="main-side__block-item main-side__block-item--currency">
-	<i class="fa fa-<?= $key; ?>" aria-hidden="true"></i>
-	<?= $item; ?>
+    <i class="fa fa-<?= $key; ?>" aria-hidden="true"></i>
+    <?= $item; ?>
 </div>
 <?php
-	endforeach;
-	unset($item, $key);
+    endforeach;
+    unset($item, $key);
 endif;
 ?>
 
 <?php if (!empty($sets['copyright'])) : ?>
 <small>
-	По данным сайта <a href="http://cbr.ru/" title="Центральный Банк России" target="_blank">Центробанка РФ</a>
+    По данным сайта <a href="http://cbr.ru/" title="Центральный Банк России" target="_blank">Центробанка РФ</a>
 </small>
 <?php endif; ?>
